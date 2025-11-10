d84d3799764f9b7b8de56902aadd620e592ae817
LiviaSobral <liviaheloisa2005@gmail.com>
2025-11-10 14:52:47 -0300

    atualizado

---
+++ AppIncrivel8/components/SettingItem.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SettingItem() {
+  return (
+    <View>
+      <Text>SettingItem</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ AppIncrivel8/components/UserCard.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function UserCard() {
+  return (
+    <View>
+      <Text>UserCard</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
